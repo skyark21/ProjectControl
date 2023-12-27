c3bdb64dd538e153a5909c35c6fd6a50fb400b8c
skyark <42156935+skyark21@users.noreply.github.com>
2023-12-27 18:28:54 +0000

    Entity abstraction is created

---
+++ src/ProjectControl.Domain/Abstractions/Entity.cs
@@ -0,0 +1,11 @@
+namespace ProjectControl.Domain.Abstractions;
+
+public abstract class Entity
+{
+    protected Entity(Guid id)
+    {
+        Id = id;
+    }
+
+    public Guid Id { get; init; }
+}
